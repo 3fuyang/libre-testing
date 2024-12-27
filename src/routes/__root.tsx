be30e4dbf5d071eb766dd710c9b21497b39008b4
fwio <fwio3193@gmail.com>
2024-12-27 17:44:24 +0800

    feat: toggle devtools

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,7 +1,17 @@
 import Layout from '@/components/layout'
 import { createRootRoute, Outlet } from '@tanstack/react-router'
-import { TanStackRouterDevtools } from '@tanstack/router-devtools'
 import '@/index.css'
+import { lazy } from 'react'
+
+const TanStackRouterDevtools =
+  process.env.NODE_ENV === 'production'
+    ? () => null // Render nothing in production
+    : lazy(() =>
+        // Lazy load in development
+        import('@tanstack/router-devtools').then((res) => ({
+          default: res.TanStackRouterDevtools,
+        })),
+      )
 
 export const Route = createRootRoute({
   component: RootComponent,
